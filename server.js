7860219c547151c540f2947c556a053e78fd55a4
unknown <devik.kota@gmail.com>
2021-10-23 09:46:49 +0530

    data fetched

--- server.js
+++ server.js
@@ -14,17 +14,47 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 //All variables
-var data;
+var git_data;
+var repos;
+var user;
 
 //All scripts
 app.get('/', (req, res)=>{
     res.render('index', {});
 });
 
+app.post("/", (req, res)=>{
+    user = req.body.user;
+    get_data();
+
+    res.redirect('/');
+});
+
 //Run server
 app.listen(process.env.PORT || 4000, ()=>{
     console.log("listening");
 });
+
+
+//fetch data
+
+async function get_data(){
+
+    try{
+        const data = await fetch("https://api.github.com/users/" + user);
+        git_data = await data.json();
+        console.log(git_data);
+    
+        const data_repo = await fetch("https://api.github.com/users/" + user + "/repos");
+        repos = await data_repo.json();
+        console.log(repos);
+    }
+    catch{
+        console.log(err);
+    }
+    
+}
+
 
 
 //API
